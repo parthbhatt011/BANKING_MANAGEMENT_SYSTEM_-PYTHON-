f46b82aaa2e71a1470349748a1979e83d0a6c3eb
PARTH BHATT <parthbhatt011@gmail.com>
2026-03-16 20:59:12 +0530

    ..

--- ui/login_window.py
+++ ui/login_window.py
@@ -17,7 +17,7 @@ def login_screen():
 
         console.clear()
         console.print(Panel("         BANKING MANAGEMENT SYSTEM      ", style="bold blue"))
-
+        console.print("welcome to Banking Management System", style="bold blue")
         console.print("1. Login")
         console.print("2. Register")
         console.print("3. Exit")
